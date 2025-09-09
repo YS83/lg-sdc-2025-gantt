2d0ab1297180b2adf18c4db4676ca669c1fac520
Youngsam Kim <verdino83@gmail.com>
2025-09-09 11:53:34 +0900

    Update lg_sdc_gantt_app.py

--- lg_sdc_gantt_app.py
+++ lg_sdc_gantt_app.py
@@ -31,7 +31,7 @@ initial_tasks = [
     {"Task": "프로그램별 안내페이지 제작", "Start": "2025-07-22", "Finish": "2025-08-25", "Resource": "Kim", "Status": "Completed"},
     {"Task": "글로벌라운지 시설 예약", "Start": "2025-07-25", "Finish": "2025-07-27", "Resource": "Kim", "Status": "Completed"},
     {"Task": "공용시설 운영 협조 요청", "Start": "2025-08-05", "Finish": "2025-09-12", "Resource": "Kim", "Status": "In progress"},
-    {"Task": "안전작업계획서 작성", "Start": "2025-08-01", "Finish": "2025-09-09", "Resource": "Kim", "Status": "In progress"},
+    {"Task": "안전작업계획서 작성", "Start": "2025-08-01", "Finish": "2025-09-09", "Resource": "Kim", "Status": "Completed"},
     {"Task": "추가 발표자 모집", "Start": "2025-08-25", "Finish": "2025-08-29", "Resource": "Kim", "Status": "Completed"},
     {"Task": "오찬 참석자 명단 확정", "Start": "2025-08-25", "Finish": "2025-08-29", "Resource": "Kim", "Status": "Completed"},
     {"Task": "오픈멘토링 준비 사항 안내", "Start": "2025-08-27", "Finish": "2025-08-27", "Resource": "Kim", "Status": "Completed"},
